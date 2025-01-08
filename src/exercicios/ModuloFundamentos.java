0283b641898495791b4273ce3ff940f41a665be0
Lucas Safeno <lugnorio@gmail.com>
2025-01-08 10:53:55 -0300

    Udemy - Java - Cod3r - 63. IF

---
+++ src/exercicios/ModuloFundamentos.java
@@ -0,0 +1,44 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ModuloFundamentos {
+ public static void main(String[] args) {
+	 Scanner entrada = new Scanner(System.in);
+	 final int FATOR = 32;
+	 /**
+	 * 1. Criar um programa que leia a temperatura em Fahrenheit e converta para Celsius
+
+	 System.out.println("Informe a temperatura em Fahrenheit : ");
+	 double temperatura = entrada.nextDouble();
+	 double resultado = (temperatura - FATOR) / 1.8;
+	 System.out.printf(temperatura+"F convertido para Celcius é  : %.2f ºC", resultado);;
+	 
+	 
+	 
+	 // Criar um programa que leia a temperatura em Celsius e converta para Fahrenheit.
+	 
+	 System.out.println("Informe a temperatura em Celsius");
+	 double celcius = entrada.nextDouble();
+	 
+	 double resultado = (celcius * 1.8) + FATOR;
+	 
+	 System.out.printf("A temperatura Celcius : " + celcius+" em Fahrenheit é igual %.2f F", resultado);
+	 
+	 
+	 // Criar um programa que leia o peso e a altura do usuário e imprima no console o IMC.
+	 
+	 System.out.println("Informe seu peso");
+	 double peso = entrada.nextDouble();
+	 
+	 System.out.println("Informe sua altura :");
+	 double altura = entrada.nextDouble();
+	 
+	 double imc = peso / (altura * altura);
+	  System.out.printf("Seu IMC é : %.2f", imc);
+	  
+	  */
+	 
+	 //
+}
+}
---
+++ src/fundamentos/Ternario.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class Ternario {
+ public static void main(String[] args) {
+	double media = 6.7;
+	String resultado = media >= 7 ? "Aprovado" : "Recuperação";
+	System.out.println(resultado);
+}
+}

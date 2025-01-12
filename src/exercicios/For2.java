30b8713d04d2ac55efe5cdb0e767a3bdccfbd4fc
Lucas Safeno <lugnorio@gmail.com>
2025-01-12 18:40:15 -0300

    Udemy - Java - Cod3r - 77. FOR #02

---
+++ src/exercicios/For2.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		// Gerar um for decrescente 
+		for(int contador = 10; contador >= 0; contador -=2)
+		{
+			System.out.printf("Contador = %d\n", contador);
+		} // for
+
+	}// main
+
+} // for1

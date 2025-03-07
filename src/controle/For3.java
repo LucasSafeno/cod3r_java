0798e5b92d2a9436ecafeed330ed99e7a111460f
Lucas Safeno <lugnorio@gmail.com>
2025-03-07 18:10:52 -0300

    77. FOR #03

--- src/controle/For3.java
+++ src/controle/For3.java
@@ -3,12 +3,20 @@ package controle;
 public class For3 {
 
 	public static void main(String[] args) {
-
+		// para acessar o i fora do laço for, deve iniciar antes do escorpo for
+		/*for(int i = 0; i < 10; i++)
+		{
+			System.out.println(i);
+		}
+		int i  = 0;
+		System.out.println("Saiu do for...");
+		System.out.println(i); */
+		
 		for(int i = 0; i < 10; i++)
 		{
 			for(int j = 0; j < 10; j++)
 			{
-				System.out.printf("[%d %d]", i, j);
+				System.out.printf("[%d  %d]", i, j);
 			}
 		}
 
